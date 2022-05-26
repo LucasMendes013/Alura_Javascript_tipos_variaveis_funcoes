ef7ab29715110a6bfa679c4379039665b066a14f
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-26 07:55:19 -0300

    commit 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 1
+const segundoNumero = 2
+const primeiraOperacao = primeiroNumero + segundoNumero
+console.log(primeiraOperacao)
+
+//numeros flutuantes (são decimais, com virgula)
+
+const primeiroFlutuante = 0.1
+const segundoFlutuante = .3
+const segundaOperacao = primeiroFlutuante + segundoFlutuante
+console.log(segundaOperacao)
