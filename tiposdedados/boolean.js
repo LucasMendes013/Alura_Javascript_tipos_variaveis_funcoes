9557af8f5646649448cd80aa02a9cd3ca66e4cc2
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-26 08:31:00 -0300

    boolean

---
+++ tiposdedados/boolean.js
@@ -0,0 +1,4 @@
+const primeiroNumero = 5
+const segundoNumero = 10
+console.log(primeiroNumero === segundoNumero)
+//se o segundoNumero fosse 5 seria true ao inves de false
--- tiposdedados/string.js
+++ tiposdedados/string.js
@@ -5,3 +5,32 @@ const citacao = "ola mundo 'ola'"
 //somando apenas string com string as frases se juntam
 const operacaoString = citacao + texto1
 console.log(operacaoString)
+
+//podemos guardar nas variaveis, simbolos também
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+//para padronizar uma comparação entre strings: (o js não entende que minusculas e maiusculas nao diferem tanto)
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+
+//Uma das formas de tratar isso é padronizando todos os inputs para o formato de texto que será comparado antes mesmo de fazer a comparação. Nesse caso, transformando todos os caracteres em letras minúsculas.
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+//para contar quantos caracteres temos naquela string, podemos usar o length
+const senha = "senha1234"
+console.log(senha.length)
