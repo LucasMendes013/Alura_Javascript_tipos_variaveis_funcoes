d5ac0ce76db49059eb4b3354840db8ee14a4ae6c
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-26 11:52:52 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+let texto = 1
+
+function dizerInformacao(texto) {
+    console.log(texto)
+}
+
+dizerInformacao(texto)
+
+//se eu coloco a let sem dizer ali em baixo o nome dela, nada acontecera
+
+//podemos reutilizar functions varias vezes
+
